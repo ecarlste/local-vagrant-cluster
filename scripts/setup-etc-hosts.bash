c9a0c0764867c62671aaabcae5ce2ea978490e90
Erik S. Carlsten <ecarlste@gmail.com>
2024-12-16 14:47:58 -0700

    fix issue with cross node communication

--- scripts/setup-etc-hosts.bash
+++ scripts/setup-etc-hosts.bash
@@ -2,6 +2,6 @@
 
 sudo apt-get update -y
 sudo echo "$IP_PREFIX$((IP_LAST_TERM_START)) controlplane" >> /etc/hosts
-for i in `seq 1 ${NUM_WORKER_NODES}`; do
+for i in `seq 1 ${WORKER_NODE_COUNT}`; do
   sudo echo "$IP_PREFIX$((IP_LAST_TERM_START+i)) node0${i}" >> /etc/hosts
 done
